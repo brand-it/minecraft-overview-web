ad49940ce9fd813d5200ea049ae76558b3856c61
Brandt Lareau <brandt.lareau@rackspace.com>
2013-01-14 15:38:20 -0600

    Update ruby and deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,9 @@ set :deploy_to, "/home/rails"
 set :user, "rails"
 set :use_sudo, false
 
+set :rvm_ruby_string, 'ruby-1.9.3-p362@minecraft_master'
+set :rvm_type, :system
+
 server "198.61.203.101", :app, :web, :db, :primary => true
 
 namespace :deploy do
