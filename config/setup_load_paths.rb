dffd14670e127fd69a7d26cfb5e949d242c03b89
Brandt Lareau <brandt.lareau@rackspace.com>
2013-01-14 21:34:05 -0600

    update deploy

--- config/setup_load_paths.rb
+++ config/setup_load_paths.rb
@@ -2,7 +2,7 @@ if ENV['MY_RUBY_HOME'] && ENV['MY_RUBY_HOME'].include?('rvm')
   begin
     rvm_path     = File.dirname(File.dirname(ENV['MY_RUBY_HOME']))
     rvm_lib_path = File.join(rvm_path, 'lib')
-    $LOAD_PATH.unshift rvm_lib_path
+    # $LOAD_PATH.unshift rvm_lib_path
     require 'rvm'
     RVM.use_from_path! File.dirname(File.dirname(__FILE__))
   rescue LoadError
@@ -11,15 +11,18 @@ if ENV['MY_RUBY_HOME'] && ENV['MY_RUBY_HOME'].include?('rvm')
   end
 end
 
-# If we're using a Bundler 1.0 beta
+# Pick the lines for your version of Bundler
+# If you're not using Bundler at all, remove all of them
+
+# Require Bundler 1.0
 ENV['BUNDLE_GEMFILE'] = File.expand_path('../Gemfile', File.dirname(__FILE__))
 require 'bundler/setup'
 
-# Or Bundler 0.9...
-if File.exist?(".bundle/environment.rb")
-  require '.bundle/environment'
-else
-  require 'rubygems'
-  require 'bundler'
-  Bundler.setup
-end
+# Require Bundler 0/9
+# if File.exist?(".bundle/environment.rb")
+#   require '.bundle/environment'
+# else
+#   require 'rubygems'
+#   require 'bundler'
+#   Bundler.setup
+# end
