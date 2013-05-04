ded61bc81f76d8202079557005cdf0e5ef9aefcb
Brandt Lareau <newdark1@gmail.com>
2013-05-04 17:34:38 -0500

    Updated Gemfiles again and change the discover.rb file

--- app/autotest/discover.rb
+++ app/autotest/discover.rb
@@ -1,20 +1,22 @@
-Autotest::Growl::clear_terminal = false
+unless Rails.env.prodution?
+  Autotest::Growl::clear_terminal = false
 
-Autotest.add_hook :initialize do |at|
+  Autotest.add_hook :initialize do |at|
 
-  %w{.git .hg .DS_Store ._* vendor}.each {|exception| at.add_exception(exception) }
-  # Gemfile.lock is causing cucumber to run infinitely. Don't watch it.
-    at.add_exception(%r{^\./Gemfile.lock})
+    %w{.git .hg .DS_Store ._* vendor}.each {|exception| at.add_exception(exception) }
+    # Gemfile.lock is causing cucumber to run infinitely. Don't watch it.
+      at.add_exception(%r{^\./Gemfile.lock})
   
-  at.add_mapping(%r%^spec/acceptance/.*_spec.rb$%, true) { |filename, _|
-    filename
-  }
+    at.add_mapping(%r%^spec/acceptance/.*_spec.rb$%, true) { |filename, _|
+      filename
+    }
 
-  at.add_mapping(%r%^app/(models|controllers|helpers|lib)/.*rb$%, true) {
-    at.files_matching %r%^spec/acceptance/.*_spec.rb$%
-  }
+    at.add_mapping(%r%^app/(models|controllers|helpers|lib)/.*rb$%, true) {
+      at.files_matching %r%^spec/acceptance/.*_spec.rb$%
+    }
 
-  at.add_mapping(%r%^app/views/(.*)$%, true) {
-    at.files_matching %r%^spec/acceptance/.*_spec.rb$%
-  }
-end
+    at.add_mapping(%r%^app/views/(.*)$%, true) {
+      at.files_matching %r%^spec/acceptance/.*_spec.rb$%
+    }
+  end
+end # unless Rails.env.prodution?
